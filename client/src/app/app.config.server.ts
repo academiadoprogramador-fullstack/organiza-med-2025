9e3197b561328b35b66f5c1f9807c4d587038eb1
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-11-06 14:47:10 -0300

    Adicionada AppShell

--- client/src/app/app.config.server.ts
+++ client/src/app/app.config.server.ts
@@ -1,12 +1,24 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRendering, withRoutes } from '@angular/ssr';
+import { ApplicationConfig, mergeApplicationConfig } from '@angular/core';
+import {
+  provideServerRendering,
+  RenderMode,
+  ServerRoute,
+  withAppShell,
+  withRoutes,
+} from '@angular/ssr';
+
 import { appConfig } from './app.config';
-import { serverRoutes } from './app.routes.server';
+import { ShellComponent } from './components/shared/shell/shell.component';
+
+export const serverRoutes: ServerRoute[] = [
+  {
+    path: '**',
+    renderMode: RenderMode.Prerender,
+  },
+];
 
 const serverConfig: ApplicationConfig = {
-  providers: [
-    provideServerRendering(withRoutes(serverRoutes))
-  ]
+  providers: [provideServerRendering(withRoutes(serverRoutes), withAppShell(ShellComponent))],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { ShellComponent } from './components/shared/shell/shell.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: ShellComponent,
+    children: [],
+  },
+];
