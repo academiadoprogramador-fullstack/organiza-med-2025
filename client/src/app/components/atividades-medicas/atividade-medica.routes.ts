040833ea1eb7223585feb2d7c9107beea7f1bf04
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-11-14 15:29:54 -0300

    Adicionado componente de Exclusão de Atividades Médicas

--- client/src/app/components/atividades-medicas/atividade-medica.routes.ts
+++ client/src/app/components/atividades-medicas/atividade-medica.routes.ts
@@ -6,7 +6,7 @@ import { PacienteService } from '../pacientes/paciente.service';
 import { AtividadeMedicaService } from './atividade-medica.service';
 import { CadastrarAtividadeMedica } from './cadastrar/cadastrar-atividade-medica';
 import { EditarAtividadeMedica } from './editar/editar-atividade-medica';
-// import { ExcluirAtividadeMedica } from './excluir/excluir-atividade-medica';
+import { ExcluirAtividadeMedica } from './excluir/excluir-atividade-medica';
 import { ListarAtividadesMedicas } from './listar/listar-atividades-medicas';
 
 export const listarAtividadesMedicasResolver = () => {
@@ -53,11 +53,11 @@ export const atividadeMedicaRoutes: Routes = [
           atividadeMedica: detalhesAtividadeMedicaResolver,
         },
       },
-      // {
-      //   path: 'excluir/:id',
-      //   component: ExcluirAtividadeMedica,
-      //   resolve: { atividadeMedica: detalhesAtividadeMedicaResolver },
-      // },
+      {
+        path: 'excluir/:id',
+        component: ExcluirAtividadeMedica,
+        resolve: { atividadeMedica: detalhesAtividadeMedicaResolver },
+      },
     ],
     providers: [PacienteService, MedicoService, AtividadeMedicaService],
   },
